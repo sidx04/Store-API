9b4e0b25c3363b7a60c52590f274caa000bf87c4
Onion-Man <siddpal04@gmail.com>
2022-12-01 23:48:49 +0530

    change in appjs

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express=require('express')
 const app=express()
-require('dotenv').config()
-
 const connectDB=require('./db/connect')
+const productsRouter=require('./routes/products')
+require('dotenv').config()
+require('express-async-errors')
 
 const notFoundMiddleware=require('./middleware/not-found')
 const errorHandlerMiddleware=require('./middleware/error-handler')
@@ -11,6 +12,8 @@ const errorHandlerMiddleware=require('./middleware/error-handler')
 app.get('/',(req,res)=>{
     res.send(`<h1>Store API</h1><a href="/api/v1/products">Products route</a>`)
 })
+
+app.use('/api/v1/products',productsRouter)
 
 //products route
 app.use(notFoundMiddleware)
@@ -22,6 +25,7 @@ const port=process.env.PORT || 3000
 const start=async()=>{
     try {
         //connect DB
+        await connectDB(process.env.MONGO_URI)
         app.listen(port,()=>{
             console.log(`Server is listening on port ${port}...`);
         })          
