ca36c5fa7850c6b113cef6aa9d1f918609b5f271
Onion-Man <siddpal04@gmail.com>
2022-12-04 00:27:02 +0530

    basic filtering operations

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,10 +1,31 @@
+const Product=require('../models/products')
+
 const getAllProductsStatic=async(req,res)=>{
-    res.status(200).json({msg:'products testing route'})
+    const products=await Product.find({featured:true,})
+    res.status(200).json({products, nbHits:products.length})
     //throw new Error('testing async errors...')
 }
 
 const getAllProducts=async(req,res)=>{
-    res.status(200).json({msg:'products route'})
+    //console.log(req.query);
+    const {featured,company,name}=req.query
+    const queryObj={}
+    
+    if (featured){
+        queryObj.featured=featured==='true'?true:false
+    }
+
+    if(company){
+        queryObj.company=company
+    }
+
+    if(name){
+        queryObj.name=name
+    }
+
+    console.log(queryObj);
+    const products=await Product.find(queryObj)
+    res.status(200).json({products, nbHits:products.length})
 }
 
 module.exports={
