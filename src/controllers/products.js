a9e8d2db2d4f98c3b0f4d96f3c7957d1ed5cdaf6
Onion-Man <siddpal04@gmail.com>
2022-12-04 16:24:13 +0530

    numeric filters

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -34,13 +34,20 @@ const getAllProductsStatic=async(req,res)=>{
      * >    .skip(2)
      * > 
      */
+
+    const products=await Product.find({price:{$gt:30}})
+        .sort('price')
+        .select('name price')
+     
     res.status(200).json({products, nbHits:products.length})
     //throw new Error('testing async errors...')
 }
 
+
+
 const getAllProducts=async(req,res)=>{
     //console.log(req.query);
-    const {featured,company,name,sort, select}=req.query
+    const {featured,company,name,sort, select, numericFilters}=req.query
     const queryObj={}
     
     if (featured){
@@ -54,18 +61,53 @@ const getAllProducts=async(req,res)=>{
     /**
      * ! use regex for non-strict filtering 
      */
-
+    
     if(name){
         queryObj.name={$regex:name,$options:'i'}
     }
+    
+    /**
+     * ! Regex numeric filters
+     */
+    if(numericFilters){
+        const operatorMap={
+            '>':'$gt',
+            '>=':'$gte',
+            '<':'$lt',
+            '<=':'$lte',
+            '=':'$eq',
+        }
 
-    //console.log(queryObj);
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g; 
+
+        let filters=numericFilters.replace(
+            regEx,(match)=>
+            `-${operatorMap[match]}-`
+            
+            /**
+             * + added hyphen to later help in splitting the array
+             */
+        )
+
+        //console.log(filters);
+
+        const options=['price', 'rating']
+        filters=filters.split(',').forEach((item)=>{
+            const [field,operator,value]=item.split('-')
+            if (options.includes(field)){
+                queryObj[field]={[operator]:Number(value)}
+            }
+        })
+    }
+
+
+    console.log(queryObj)
+
+    let result= Product.find(queryObj)
     
     /**
      *  ! Sorting
-     */
-
-      let result= Product.find(queryObj)
+     */      
       
       if(sort){
         const sortList=sort.split(',').join(' ')
@@ -93,8 +135,8 @@ const getAllProducts=async(req,res)=>{
       const page=Number(req.query.page)||1
       const limit=Number(req.query.limit)||10
       const skip=(page-1)*limit
-      
-      
+
+
       result=result.skip(skip).limit(limit)
       
       const products=await result
